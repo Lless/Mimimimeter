df2774ee6838cdd4885ba515ee8575a346f78555
Igor Shangin <igorsan1@mail.ru>
2020-02-15 13:23:28 +0300

    Hello, world springboot app

---
+++ src/main/java/com/github/lless/mimimimeter/MimimimeterApp.java
@@ -0,0 +1,11 @@
+package com.github.lless.mimimimeter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MimimimeterApp {
+    public static void main(String[] args) {
+        SpringApplication.run(MimimimeterApp.class, args);
+    }
+}
